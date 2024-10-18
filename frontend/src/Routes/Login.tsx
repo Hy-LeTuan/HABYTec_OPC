75a61dced6fb3ab155386d9a2e5518d226d8ea60
Hy-LeTuan <letuanhy1507@gmail.com>
2024-10-18 11:17:52 +0700

    FE: update login and signup

--- frontend/src/Routes/Login.tsx
+++ frontend/src/Routes/Login.tsx
@@ -66,7 +66,7 @@ function Login() {
 	return (
 		<>
 			<Section color={"radial"} className="h-dvh">
-				<div className="w-full pt-10 relative">
+				<div className="w-full h-full pt-10 relative">
 					<Link to={"/"} className="absolute left-16">
 						<img
 							className="w-10 h-10"
@@ -74,103 +74,109 @@ function Login() {
 							alt="white habytec logo"
 						/>
 					</Link>
-					<div className="px-20 py-10 w-[780px] mx-auto border-white/50 border-2 bg-white/5 rounded-lg shadow-md shadow-white/5">
-						<div className="w-full text-center flex flex-col gap-6 justify-center items-center">
-							<h1 className="text-white font-medium text-4xl tracking-widest">
-								SIGN IN
-							</h1>
-							<div className="w-full flex flex-col justify-center items-center gap-4">
-								<FormInputField
-									name={"email"}
-									type={"email"}
-									label={"Your Email"}
-									placeholder={"Enter your email"}
-									errorMessage={errorMessage["email"]}
-									isError={isError["email"]}
-									onChangeFunction={onUserLoginInputChange}
-									value={userLoginInput["email"]}
-									className={""}
-								/>
-								<FormInputField
-									name={"password"}
-									type={"password"}
-									label={"Your Password"}
-									placeholder={"Enter your password"}
-									errorMessage={errorMessage["password"]}
-									isError={isError["password"]}
-									onChangeFunction={onUserLoginInputChange}
-									value={userLoginInput["password"]}
-									className={""}
-								/>
-							</div>
-							<div className="w-full text-left text-base text-white">
-								<Link to={"/forget-password"}>
-									Forget your password?
-								</Link>
-							</div>
-							<Button
-								className={
-									"transition-colors duration-300 hover:bg-indigo-900 w-full rounded-lg bg-accent shadow-black/10 shadow-md py-3"
-								}
-								onClick={onUserLoginInputSubmit}>
-								<h3 className="text-white text-xl font-medium">
-									Sign In
-								</h3>
-							</Button>
-							<div className="w-full text-center text-base text-neutral-400">
-								<p>- Or sign in with -</p>
-							</div>
-							<div className="flex flex-row justify-between w-full items-center">
-								<Button
-									className={
-										"transition-colors flex flex-row justify-center items-center gap-3 px-5 py-2 rounded-md border-white border-[0.5px] hover:bg-white/5"
-									}>
-									<FaFacebook
-										color="#0866FF"
-										className="w-6 h-6"
+					<div className="h-full flex flex-col justify-center items-center">
+						<div className="px-20 py-10 w-[780px] mx-auto border-white/50 border-2 bg-white/5 rounded-lg shadow-md shadow-white/5">
+							<div className="w-full text-center flex flex-col gap-6 justify-center items-center">
+								<h1 className="text-white font-medium text-4xl tracking-widest">
+									SIGN IN
+								</h1>
+								<div className="w-full flex flex-col justify-center items-center gap-4">
+									<FormInputField
+										name={"email"}
+										type={"email"}
+										label={"Your Email"}
+										placeholder={"Enter your email"}
+										errorMessage={errorMessage["email"]}
+										isError={isError["email"]}
+										onChangeFunction={
+											onUserLoginInputChange
+										}
+										value={userLoginInput["email"]}
+										className={""}
 									/>
-									<h4 className="text-white text-base">
-										Facebook
-									</h4>
-								</Button>
-								<Button
-									className={
-										"w-40 transition-colors flex flex-row justify-center items-center gap-3 px-5 py-2 rounded-md border-white border-[0.5px] hover:bg-white/5"
-									}>
-									<img
-										src="/src/assets/icons/google_logo.svg"
-										alt="Google icon"
-										className="w-6 h-6"
+									<FormInputField
+										name={"password"}
+										type={"password"}
+										label={"Your Password"}
+										placeholder={"Enter your password"}
+										errorMessage={errorMessage["password"]}
+										isError={isError["password"]}
+										onChangeFunction={
+											onUserLoginInputChange
+										}
+										value={userLoginInput["password"]}
+										className={""}
 									/>
-									<h4 className="text-white text-base">
-										Google
-									</h4>
-								</Button>
-								<Button
-									className={
-										"w-40 transition-colors flex flex-row justify-center items-center gap-3 px-5 py-2 rounded-md border-white border-[0.5px] hover:bg-white/5"
-									}>
-									<img
-										src="/src/assets/icons/apple_logo.svg"
-										alt="Google icon"
-										className="w-6 h-6"
-									/>
-									<h4 className="text-white text-base">
-										Apple
-									</h4>
-								</Button>
-							</div>
-							<div className="w-full text-center text-base">
-								<p>
-									<span className="text-neutral-400">
-										Don't have an account? &gt;{" "}
-									</span>
-									<Link
-										to={"/register"}
-										className="text-accent font-medium">
-										Register here
+								</div>
+								<div className="w-full text-left text-base text-white">
+									<Link to={"/forget-password"}>
+										Forget your password?
 									</Link>
-								</p>
+								</div>
+								<Button
+									className={
+										"transition-colors duration-300 hover:bg-indigo-900 w-full rounded-lg bg-accent shadow-black/10 shadow-md py-3"
+									}
+									onClick={onUserLoginInputSubmit}>
+									<h3 className="text-white text-xl font-medium">
+										Sign In
+									</h3>
+								</Button>
+								<div className="w-full text-center text-base text-neutral-400">
+									<p>- Or sign in with -</p>
+								</div>
+								<div className="flex flex-row justify-between w-full items-center">
+									<Button
+										className={
+											"transition-colors flex flex-row justify-center items-center gap-3 px-5 py-2 rounded-md border-white border-[0.5px] hover:bg-white/5"
+										}>
+										<FaFacebook
+											color="#0866FF"
+											className="w-6 h-6"
+										/>
+										<h4 className="text-white text-base">
+											Facebook
+										</h4>
+									</Button>
+									<Button
+										className={
+											"w-40 transition-colors flex flex-row justify-center items-center gap-3 px-5 py-2 rounded-md border-white border-[0.5px] hover:bg-white/5"
+										}>
+										<img
+											src="/src/assets/icons/google_logo.svg"
+											alt="Google icon"
+											className="w-6 h-6"
+										/>
+										<h4 className="text-white text-base">
+											Google
+										</h4>
+									</Button>
+									<Button
+										className={
+											"w-40 transition-colors flex flex-row justify-center items-center gap-3 px-5 py-2 rounded-md border-white border-[0.5px] hover:bg-white/5"
+										}>
+										<img
+											src="/src/assets/icons/apple_logo.svg"
+											alt="Google icon"
+											className="w-6 h-6"
+										/>
+										<h4 className="text-white text-base">
+											Apple
+										</h4>
+									</Button>
+								</div>
+								<div className="w-full text-center text-base">
+									<p>
+										<span className="text-neutral-400">
+											Don't have an account? &gt;{" "}
+										</span>
+										<Link
+											to={"/register"}
+											className="text-accent font-medium">
+											Register here
+										</Link>
+									</p>
+								</div>
 							</div>
 						</div>
 					</div>
--- frontend/src/Routes/Register.tsx
+++ frontend/src/Routes/Register.tsx
@@ -78,7 +78,7 @@ function Register() {
 	return (
 		<>
 			<Section color={"radial"} className="h-dvh">
-				<div className="w-full pt-10 relative">
+				<div className="w-full h-full pt-10 relative">
 					<Link to={"/"} className="absolute left-16">
 						<img
 							className="w-10 h-10"
@@ -86,80 +86,94 @@ function Register() {
 							alt="white habytec logo"
 						/>
 					</Link>
-					<div className="px-20 py-10 w-[780px] mx-auto border-white/50 border-2 bg-white/5 rounded-lg shadow-md shadow-white/5">
-						<div className="w-full text-center flex flex-col gap-8 justify-center items-center">
-							<h1 className="text-white font-medium text-4xl tracking-widest">
-								SIGN UP
-							</h1>
-							<div className="w-full flex flex-col justify-center items-center gap-4">
-								<FormInputField
-									name={"email"}
-									type={"email"}
-									label={"Your email"}
-									placeholder={"Enter your email"}
-									errorMessage={errorMessage["email"]}
-									isError={isError["email"]}
-									onChangeFunction={onUserRegisterInputChange}
-									value={userRegisterInput["email"]}
-									className={""}
-								/>
-								<FormInputField
-									name={"username"}
-									type={"text"}
-									label={"Your username"}
-									placeholder={"Enter your username"}
-									errorMessage={errorMessage["username"]}
-									isError={isError["username"]}
-									onChangeFunction={onUserRegisterInputChange}
-									value={userRegisterInput["username"]}
-									className={""}
-								/>
-								<FormInputField
-									name={"password"}
-									type={"password"}
-									label={"Your password"}
-									placeholder={"Enter your password"}
-									errorMessage={errorMessage["password"]}
-									isError={isError["password"]}
-									onChangeFunction={onUserRegisterInputChange}
-									value={userRegisterInput["password"]}
-									className={""}
-								/>
-								<FormInputField
-									name={"confirm_password"}
-									type={"password"}
-									label={"Confirm your password"}
-									placeholder={"Enter your password again"}
-									errorMessage={
-										errorMessage["confirm_password"]
-									}
-									isError={isError["confirm_password"]}
-									onChangeFunction={onUserRegisterInputChange}
-									value={
-										userRegisterInput["confirm_password"]
-									}
-									className={""}
-								/>
-							</div>
-							<Button
-								className={
-									"transition-colors duration-300 hover:bg-indigo-900 w-full rounded-lg bg-accent shadow-black/10 shadow-md py-3"
-								}>
-								<h3 className="text-white text-xl font-medium">
-									Sign In
-								</h3>
-							</Button>
-							<div className="w-full text-center text-base">
-								<p>
-									<span className="text-neutral-400">
-										Already have an account? &gt;{" "}
-									</span>
-									<Link
-										to={"/login"}
-										className="text-accent font-medium">
-										Login here
-									</Link>
-								</p>
+					<div className="h-full flex flex-col justify-center">
+						<div className="px-20 py-10 w-[780px] mx-auto border-white/50 border-2 bg-white/5 rounded-lg shadow-md shadow-white/5">
+							<div className="w-full text-center flex flex-col gap-8 justify-center items-center">
+								<h1 className="text-white font-medium text-4xl tracking-widest">
+									SIGN UP
+								</h1>
+								<div className="w-full flex flex-col justify-center items-center gap-6">
+									<FormInputField
+										name={"email"}
+										type={"email"}
+										label={"Your email"}
+										placeholder={"Enter your email"}
+										errorMessage={errorMessage["email"]}
+										isError={isError["email"]}
+										onChangeFunction={
+											onUserRegisterInputChange
+										}
+										value={userRegisterInput["email"]}
+										className={""}
+									/>
+									<FormInputField
+										name={"username"}
+										type={"text"}
+										label={"Your username"}
+										placeholder={"Enter your username"}
+										errorMessage={errorMessage["username"]}
+										isError={isError["username"]}
+										onChangeFunction={
+											onUserRegisterInputChange
+										}
+										value={userRegisterInput["username"]}
+										className={""}
+									/>
+									<FormInputField
+										name={"password"}
+										type={"password"}
+										label={"Your password"}
+										placeholder={"Enter your password"}
+										errorMessage={errorMessage["password"]}
+										isError={isError["password"]}
+										onChangeFunction={
+											onUserRegisterInputChange
+										}
+										value={userRegisterInput["password"]}
+										className={""}
+									/>
+									<FormInputField
+										name={"confirm_password"}
+										type={"password"}
+										label={"Confirm your password"}
+										placeholder={
+											"Enter your password again"
+										}
+										errorMessage={
+											errorMessage["confirm_password"]
+										}
+										isError={isError["confirm_password"]}
+										onChangeFunction={
+											onUserRegisterInputChange
+										}
+										value={
+											userRegisterInput[
+												"confirm_password"
+											]
+										}
+										className={""}
+									/>
+								</div>
+								<Button
+									className={
+										"transition-colors duration-300 hover:bg-indigo-900 w-full rounded-lg bg-accent shadow-black/10 shadow-md py-3"
+									}>
+									<h3 className="text-white text-xl font-medium">
+										Sign In
+									</h3>
+								</Button>
+								<div className="w-full text-center text-base">
+									<p>
+										<span className="text-neutral-400">
+											Already have an account? &gt;{" "}
+										</span>
+										<Link
+											to={"/login"}
+											className="text-accent font-medium">
+											Login here
+										</Link>
+									</p>
+								</div>
 							</div>
 						</div>
 					</div>
